9774f8ab52d3877397a9fbedb93fbd5586edaa32
David Baumgarten <grtn316@hotmail.com>
2021-04-14 17:10:11 +0000

    set root_parent_id to null

--- DefaultDeployment/variables.tf
+++ DefaultDeployment/variables.tf
@@ -2,7 +2,7 @@
 
 variable "root_parent_id" { #REQUIRED: The root_parent_id is used to specify where to set the root for all Landing Zone deployments. Usually the Tenant ID when deploying the core Enterprise-scale Landing Zones.
   type    = string
-  default = ""
+  default = null
 }
 
 variable "root_id" { #OPTIONAL: If specified, will set a custom Name (ID) value for the Enterprise-scale "root" Management Group, and append this to the ID for all core Enterprise-scale Management Groups.
