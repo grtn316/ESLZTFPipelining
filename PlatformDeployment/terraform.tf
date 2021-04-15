722e388093314676a3d3ca384109e07d0b5dc4bf
Jennelle Crothers <jkcrothers@hotmail.com>
2021-04-15 12:24:37 -0700

    Update terraform.tf

--- PlatformDeployment/terraform.tf
+++ PlatformDeployment/terraform.tf
@@ -1,11 +1,31 @@
 # Configure Terraform to set the required AzureRM provider
 # version and features{} block.
 
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">= 2.46.1"
+#     }
+#   }
+
+#   backend "remote" {
+#     hostname = "app.terraform.io"
+#     organization = "jcroth"
+#     workspaces {
+#       name = "platform"
+#     }
+#   }
+# }
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.46.1"
+    }
+    random = {
+      version = ">=3.0"
     }
   }
 
