f66b665cc9acd8c4c613aee69a3c2ab0c613b330
Jennelle Crothers <jkcrothers@hotmail.com>
2021-04-14 20:48:00 -0700

    temp remove direction2

--- TFModules/networking/peering/peering.tf
+++ TFModules/networking/peering/peering.tf
@@ -14,15 +14,15 @@ resource "azurerm_virtual_network_peering" "direction1" {
   
 }
 
-resource "azurerm_virtual_network_peering" "direction2" {
-  name                         = "${var.netB_name}-to-${var.netA_name}"
-  resource_group_name          = var.resource_group_nameB
-  virtual_network_name         = var.netB_name
-  remote_virtual_network_id    = var.netA_id
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = false
-  allow_gateway_transit        = false
-  use_remote_gateways          = false
- # provider = azurerm.connectivity
+# resource "azurerm_virtual_network_peering" "direction2" {
+#   name                         = "${var.netB_name}-to-${var.netA_name}"
+#   resource_group_name          = var.resource_group_nameB
+#   virtual_network_name         = var.netB_name
+#   remote_virtual_network_id    = var.netA_id
+#   allow_virtual_network_access = true
+#   allow_forwarded_traffic      = false
+#   allow_gateway_transit        = false
+#   use_remote_gateways          = false
+#  # provider = azurerm.connectivity
 
-}
+# }
