5a936507843c3c11359c5329f9d04c37f6d4b70a
Jennelle Crothers <jkcrothers@hotmail.com>
2021-04-15 11:18:54 -0700

    Corrected variables

--- TFModules/networking/vnet-subnet/vnet-subnet.tf
+++ TFModules/networking/vnet-subnet/vnet-subnet.tf
@@ -10,14 +10,14 @@ resource "azurerm_network_security_group" "nsg" {
 resource "azurerm_virtual_network" "vnet" {
   name                 = "default"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
+  location            = var.location
   address_space       = [var.address_space] 
   dns_servers         = var.dns_servers
   # tags                = var.tags
 
   subnet {
     name           = "default"
-    address_prefix = [var.default_subnet_prefix]
+    address_prefix = "${var.default_subnet_prefix}"
     security_group = azurerm_network_security_group.nsg.id
   }
 
