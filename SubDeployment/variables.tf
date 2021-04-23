0804375d0f20c491c96ae925cb97664c76d4ccbf
David Baumgarten <grtn316@hotmail.com>
2021-04-23 13:08:04 +0000

    update lzdeploy and subdeploy with parameters

---
+++ SubDeployment/variables.tf
@@ -0,0 +1,9 @@
+variable "billing_account_name" {
+  description = "Billing Account Name"
+  default     = 00000000
+}
+
+variable "enrollment_account_name" {
+  description = "Enrollment Account Name"
+  default     = "ABC"
+}
