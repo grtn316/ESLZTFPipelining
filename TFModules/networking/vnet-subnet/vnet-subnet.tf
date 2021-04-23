74de0e9122ca4ce4da545885dc1c7d31ef771593
Jennelle Crothers <jkcrothers@hotmail.com>
2021-04-23 09:31:14 -0700

    minor edits

--- TFModules/networking/vnet-subnet/vnet-subnet.tf
+++ TFModules/networking/vnet-subnet/vnet-subnet.tf
@@ -8,7 +8,7 @@ resource "azurerm_network_security_group" "nsg" {
 
 
 resource "azurerm_virtual_network" "vnet" {
-  name                 = "default"
+  name                 = var.vnet_name
   resource_group_name  = var.resource_group_name
   location            = var.location
   address_space       = [var.address_space] 
