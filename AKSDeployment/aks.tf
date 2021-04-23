bdca7251cca29af37819056c1b2c86d21a388c8a
Jennelle Crothers <jkcrothers@hotmail.com>
2021-04-23 09:39:35 -0700

    fixed naming and variables

--- AKSDeployment/aks.tf
+++ AKSDeployment/aks.tf
@@ -18,8 +18,8 @@ resource "random_integer" "deployment" {
 }
 
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${random_integer.deployment.result}-rg"
+resource "azurerm_resource_group" "aks" {
+  name     = "${random_integer.deployment.result}-aks-rg"
   location = data.terraform_remote_state.existing-lz.outputs.rg_location
 }
 
@@ -28,9 +28,9 @@ module "private_aks" {
   source = "../TFModules//aks-private"
 
   resource_group_name = azurerm_resource_group.rg.name
-  location            = data.terraform_remote_state.existing-lz.outputs.rg_location
+  location            = azurerm_resource_group.rg.location
   prefix              = "aks-${random_integer.deployment.result}"
-  subnet_id = data.terraform_remote_state.existing-.outputs.lz_default_subnet_id
+  subnet_id = data.terraform_remote_state.existing-lz.outputs.lz_default_subnet_id
 
 
 }
