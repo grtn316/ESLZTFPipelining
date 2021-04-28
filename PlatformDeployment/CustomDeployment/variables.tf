a88af0c627296a25298b77c8ee374eae32c76080
David Baumgarten <grtn316@hotmail.com>
2021-04-28 18:46:55 +0000

    add parameter for custom landing zone structure

--- PlatformDeployment/CustomDeployment/variables.tf
+++ PlatformDeployment/CustomDeployment/variables.tf
@@ -42,6 +42,122 @@ variable "custom_landing_zones" { #OPTIONAL: If specified, will deploy additiona
   default = {}
 }
 
+variable "root_id_3_custom_landing_zones" { #OPTIONAL: If specified, will deploy additional Management Groups alongside Enterprise-scale core Management Groups.
+  type    = map(object({ display_name = string, parent_management_group_id = string, subscription_ids = list(string), archetype_config = object({ archetype_id = string, parameters = any, access_control = any }) }))
+  default = {
+    "main" = {
+      display_name               = "CustomerRoot"
+      parent_management_group_id = "${data.azurerm_client_config.current.tenant_id}"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "main"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "usa" = {
+      display_name               = "USA"
+      parent_management_group_id = "main"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "usa"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "uk" = {
+      display_name               = "UK"
+      parent_management_group_id = "main"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "uk"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "decommissioned" = {
+      display_name               = "Decommissioned"
+      parent_management_group_id = "usa"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "default_empty"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "sandbox" = {
+      display_name               = "Sandbox"
+      parent_management_group_id = "usa"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "sandbox"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "core" = {
+      display_name               = "Core"
+      parent_management_group_id = "usa"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "default_empty"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "security" = {
+      display_name               = "Security"
+      parent_management_group_id = "core"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "default_empty"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "iam" = {
+      display_name               = "IAM"
+      parent_management_group_id = "core"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "default_empty"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "networking" = {
+      display_name               = "Networking"
+      parent_management_group_id = "core"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "networking"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "prod" = {
+      display_name               = "PROD"
+      parent_management_group_id = "usa"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "prod"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "nonprod" = {
+      display_name               = "NonPROD"
+      parent_management_group_id = "usa"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "nonprod"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+  }
+}
+
 variable "default_location" { #OPTIONAL: If specified, will use set the default location used for resource deployments where needed.
   type    = string
   default = "eastus"
