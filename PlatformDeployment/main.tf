e3ae7ad7e97b801d63f8a8876d360daa31666cdf
David Baumgarten <grtn316@hotmail.com>
2021-05-05 14:25:35 +0000

    align branch with main

--- PlatformDeployment/main.tf
+++ PlatformDeployment/main.tf
@@ -8,18 +8,19 @@ data "azurerm_client_config" "current" {}
 # Declare the Terraform Module for Cloud Adoption Framework
 # Enterprise-scale and provide a base configuration.
 
+#This module deploys the default Management Group structure
 module "enterprise_scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
-  version = "0.1.2"
+  version = "0.2.0"
 
   #Input Variables: https://registry.terraform.io/modules/Azure/caf-enterprise-scale/azurerm/latest?tab=inputs
 
   root_parent_id = var.root_parent_id == null ? data.azurerm_client_config.current.tenant_id : var.root_parent_id #REQUIRED: The root_parent_id is used to specify where to set the root for all Landing Zone deployments. Usually the Tenant ID when deploying the core Enterprise-scale Landing Zones.
-  root_id        = var.root_id   #OPTIONAL: If specified, will set a custom Name (ID) value for the Enterprise-scale "root" Management Group, and append this to the ID for all core Enterprise-scale Management Groups.
-  root_name      = var.root_name #OPTIONAL: If specified, will set a custom DisplayName value for the Enterprise-scale "root" Management Group.
+  root_id        = var.root_id                                                                                    #OPTIONAL: If specified, will set a custom Name (ID) value for the Enterprise-scale "root" Management Group, and append this to the ID for all core Enterprise-scale Management Groups.
+  root_name      = var.root_name                                                                                  #OPTIONAL: If specified, will set a custom DisplayName value for the Enterprise-scale "root" Management Group.
 
-  archetype_config_overrides = var.archetype_config_overrides == null ? {} : var.archetype_config_overrides                               # OPTIONAL: If specified, will set custom Archetype configurations to the default Enterprise-scale Management Groups.
-  create_duration_delay = var.create_duration_delay == null ? { #OPTIONAL: Used to tune terraform apply when faced with errors caused by API caching or eventual consistency. Sets a custom delay period after creation of the specified resource type.
+  archetype_config_overrides = var.archetype_config_overrides == null ? {} : var.archetype_config_overrides # OPTIONAL: If specified, will set custom Archetype configurations to the default Enterprise-scale Management Groups.
+  create_duration_delay = var.create_duration_delay == null ? {                                             #OPTIONAL: Used to tune terraform apply when faced with errors caused by API caching or eventual consistency. Sets a custom delay period after creation of the specified resource type.
     azurerm_management_group      = "30s"
     azurerm_policy_assignment     = "30s"
     azurerm_policy_definition     = "30s"
@@ -28,10 +29,12 @@ module "enterprise_scale" {
     azurerm_role_definition       = "60s"
   } : var.create_duration_delay
 
-  custom_landing_zones      = var.custom_landing_zones == null ? {} : var.custom_landing_zones           #OPTIONAL: If specified, will deploy additional Management Groups alongside Enterprise-scale core Management Groups.
+  custom_landing_zones      = var.custom_landing_zones == null ? {} : var.custom_landing_zones              #OPTIONAL: If specified, will deploy additional Management Groups alongside Enterprise-scale core Management Groups.
   default_location          = var.default_location == null ? "eastus" : var.default_location                #OPTIONAL: If specified, will use set the default location used for resource deployments where needed.
   deploy_core_landing_zones = var.deploy_core_landing_zones == null ? true : var.deploy_core_landing_zones  #OPTIONAL: If set to true, will include the core Enterprise-scale Management Group hierarchy.
   deploy_demo_landing_zones = var.deploy_demo_landing_zones == null ? false : var.deploy_demo_landing_zones #OPTIONAL: If set to true, will include the demo "Landing Zone" Management Groups.
+
+  deploy_management_resources = false #disable and deploy in Custom module
 
   destroy_duration_delay = var.destroy_duration_delay == null ? { #OPTIONAL: Used to tune terraform deploy when faced with errors caused by API caching or eventual consistency. Sets a custom delay period after destruction of the specified resource type.
     azurerm_management_group      = "0s"
@@ -45,5 +48,48 @@ module "enterprise_scale" {
   library_path = var.library_path == null ? "" : var.library_path #OPTIONAL: If specified, sets the path to a custom library folder for archetype artefacts.
 
   subscription_id_overrides = var.subscription_id_overrides == null ? {} : var.subscription_id_overrides #OPTIONAL: If specified, will be used to assign subscription_ids to the default Enterprise-scale Management Groups.
-  template_file_variables   = var.template_file_variables == null ? {} : var.template_file_variables     #OPTIONAL: If specified, provides the ability to define custom template variables used when reading in template files from the built-in and custom library_path.
+
+  #Add subscription overrides below or include in custom_landing_zones definition
+  #   # subscription_id_overrides = {
+  #   #   root           = []
+  #   #   decommissioned = []
+  #   #   sandboxes      = []
+  #   #   landing-zones  = []
+  #   #   platform       = []
+  #   #   connectivity   = []
+  #   #   management     = []
+  #   #   identity       = []
+  #   #   demo-corp      = []
+  #   #   demo-online    = []
+  #   #   demo-sap       = []
+  #   # }
+
+
+  template_file_variables = var.template_file_variables == null ? {} : var.template_file_variables #OPTIONAL: If specified, provides the ability to define custom template variables used when reading in template files from the built-in and custom library_path.
+}
+
+#This module deploys a compeltely custom Management Group structure
+module "custom_root_id" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "0.2.0"
+
+  root_parent_id = data.azurerm_client_config.current.tenant_id
+  root_id        = var.custom_root_id
+  root_name      = "${var.root_name} Custom"
+  library_path   = "${path.root}/lib"
+
+  default_location          = var.default_location == null ? "eastus" : var.default_location                #OPTIONAL: If specified, will use set the default location used for resource deployments where needed.
+  deploy_core_landing_zones = false
+  deploy_demo_landing_zones = false
+
+  #Subscription id context for TF Apply command must be match the subscription_id_management
+  deploy_management_resources    = true
+  configure_management_resources = var.configure_management_resources
+  subscription_id_management     = data.azurerm_client_config.current.subscription_id
+
+
+  #Defines the management group structure, policy and subscription relationships
+  custom_landing_zones = var.custom_root_id_custom_landing_zones
+
+
 }
--- PlatformDeployment/variables.tf
+++ PlatformDeployment/variables.tf
@@ -10,9 +10,139 @@ variable "root_id" { #OPTIONAL: If specified, will set a custom Name (ID) value 
   default = "es"
 }
 
+variable "custom_root_id" {
+  type    = string
+  default = "main" #This name needs to match the root id name in the module custom LZ config
+}
+
 variable "root_name" { #OPTIONAL: If specified, will set a custom DisplayName value for the Enterprise-scale "root" Management Group.
   type    = string
   default = "ESLZ Root"
+}
+
+variable "deploy_management_resources" {
+  type        = bool
+  description = "If set to true, will deploy the \"Management\" landing zone resources into the current Subscription context."
+  default     = false
+}
+
+variable "configure_management_resources" {
+  type = object({
+    settings = object({
+      log_analytics = object({
+        enabled = bool
+        config = object({
+          retention_in_days                           = number
+          enable_monitoring_for_arc                   = bool
+          enable_monitoring_for_vm                    = bool
+          enable_monitoring_for_vmss                  = bool
+          enable_solution_for_agent_health_assessment = bool
+          enable_solution_for_anti_malware            = bool
+          enable_solution_for_azure_activity          = bool
+          enable_solution_for_change_tracking         = bool
+          enable_solution_for_service_map             = bool
+          enable_solution_for_sql_assessment          = bool
+          enable_solution_for_updates                 = bool
+          enable_solution_for_vm_insights             = bool
+          enable_sentinel                             = bool
+        })
+      })
+      security_center = object({
+        enabled = bool
+        config = object({
+          email_security_contact             = string
+          enable_defender_for_acr            = bool
+          enable_defender_for_app_services   = bool
+          enable_defender_for_arm            = bool
+          enable_defender_for_dns            = bool
+          enable_defender_for_key_vault      = bool
+          enable_defender_for_kubernetes     = bool
+          enable_defender_for_servers        = bool
+          enable_defender_for_sql_servers    = bool
+          enable_defender_for_sql_server_vms = bool
+          enable_defender_for_storage        = bool
+        })
+      })
+    })
+    location = any
+    tags     = any
+    advanced = any
+  })
+  description = "If specified, will customize the \"Management\" landing zone resources."
+  default = {
+    settings = {
+      log_analytics = {
+        enabled = true
+        config = {
+          retention_in_days                           = 30
+          enable_monitoring_for_arc                   = true
+          enable_monitoring_for_vm                    = true
+          enable_monitoring_for_vmss                  = true
+          enable_solution_for_agent_health_assessment = true
+          enable_solution_for_anti_malware            = true
+          enable_solution_for_azure_activity          = true
+          enable_solution_for_change_tracking         = true
+          enable_solution_for_service_map             = true
+          enable_solution_for_sql_assessment          = true
+          enable_solution_for_updates                 = true
+          enable_solution_for_vm_insights             = true
+          enable_sentinel                             = true
+        }
+      }
+      security_center = {
+        enabled = true
+        config = {
+          email_security_contact             = "security_contact@replace_me"
+          enable_defender_for_acr            = true
+          enable_defender_for_app_services   = true
+          enable_defender_for_arm            = true
+          enable_defender_for_dns            = true
+          enable_defender_for_key_vault      = true
+          enable_defender_for_kubernetes     = true
+          enable_defender_for_servers        = true
+          enable_defender_for_sql_servers    = true
+          enable_defender_for_sql_server_vms = true
+          enable_defender_for_storage        = true
+        }
+      }
+    }
+    location = null
+    tags     = null
+    advanced = null
+  }
+}
+
+variable "subscription_id_connectivity" {
+  type        = string
+  description = "If specified, identifies the Platform subscription for \"Connectivity\" for resource deployment and correct placement in the Management Group hierarchy."
+  default     = ""
+
+  validation {
+    condition     = can(regex("^[a-z0-9-]{36}$", var.subscription_id_connectivity)) || var.subscription_id_connectivity == ""
+    error_message = "Value must be a valid Subscription ID (GUID)."
+  }
+}
+
+variable "subscription_id_identity" {
+  type        = string
+  description = "If specified, identifies the Platform subscription for \"Identity\" for resource deployment and correct placement in the Management Group hierarchy."
+  default     = ""
+
+  validation {
+    condition     = can(regex("^[a-z0-9-]{36}$", var.subscription_id_identity)) || var.subscription_id_identity == ""
+    error_message = "Value must be a valid Subscription ID (GUID)."
+  }
+}
+
+variable "custom_subscription_id_management" {
+  type        = string
+  description = "If specified, identifies the Platform subscription for \"Management\" for resource deployment and correct placement in the Management Group hierarchy."
+  default     = ""
+
+  validation {
+    condition     = can(regex("^[a-z0-9-]{36}$", var.custom_subscription_id_management)) || var.custom_subscription_id_management == ""
+    error_message = "Value must be a valid Subscription ID (GUID)."
+  }
 }
 
 variable "archetype_config_overrides" { # OPTIONAL: If specified, will set custom Archetype configurations to the default Enterprise-scale Management Groups.
@@ -33,6 +163,11 @@ variable "create_duration_delay" { #OPTIONAL: Used to tune terraform apply when 
 }
 
 variable "custom_landing_zones" { #OPTIONAL: If specified, will deploy additional Management Groups alongside Enterprise-scale core Management Groups.
+  type    = map(object({ display_name = string, parent_management_group_id = string, subscription_ids = list(string), archetype_config = object({ archetype_id = string, parameters = any, access_control = any }) }))
+  default = {}
+}
+
+variable "custom_root_id_custom_landing_zones" { #OPTIONAL: If specified, will deploy additional Management Groups alongside Enterprise-scale core Management Groups.
   type    = map(object({ display_name = string, parent_management_group_id = string, subscription_ids = list(string), archetype_config = object({ archetype_id = string, parameters = any, access_control = any }) }))
   default = {}
 }
@@ -86,4 +221,10 @@ variable "subscription_id_overrides" { #OPTIONAL: If specified, will be used to 
 variable "template_file_variables" { #OPTIONAL: If specified, provides the ability to define custom template variables used when reading in template files from the built-in and custom library_path.
   type    = map(any)
   default = {}
+}
+
+variable "custom_policy_roles" {
+  type        = map(list(string))
+  description = "If specified, the custom_policy_roles variable overrides which Role Definition ID(s) (value) to assign for Policy Assignments with a Managed Identity, if the assigned \"policyDefinitionId\" (key) is included in this variable."
+  default     = {}
 }
